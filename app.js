2a0aebbaee085e42c818493203b1e05e267880ee
unknown <arsen_sandic@hotmail.com>
2019-12-02 19:53:32 +0100

    a

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var mysql = require("mysql");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var port = 3000;
 
 var app = express();
-var port = 3000;
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,20 +39,20 @@ app.use(function(err, req, res, next) {
 });
 
 //Database connection
-app.use(function(req, res, next){
-	res.locals.connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'pgraf_glas',
-		password: 'Fnligvu5abca@',
-		database: 'PgrafDB'
-	});
-	res.locals.connect();
-	next();
+app.use(function (req, res, next) {
+  res.locals.connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'pgraf_glas',
+    password: 'Fnligvu5abca@',
+    database: 'PgrafDB'
+  });
+  res.locals.connect();
+  next();
 });
 
 app.listen(port, () => {
-	console.log(`Listening to requests on http://localhost:${port}`);
-  });
-  
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.get('/', function(req, res, next) {
 	res.locals.connection.query('SELECT * from user', function (error, results) {
 		if (error) throw error;
@@ -25,5 +24,4 @@ router.post('/', function(req, res, next){
     }));
   });
 });
-
 module.exports = router;

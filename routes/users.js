d8ea42eed39be59aebd0b30aa8663eb81bf4dfae
unknown <arsen_sandic@hotmail.com>
2019-12-04 00:02:07 +0100

    a

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var connection = require('./db');
+var connection = require('../db');
 
 
 router.get('/', function(req, res, next) {

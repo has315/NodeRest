aad4a5e8a96ef77a1c25a3ddb2f502650d53908e
unknown <arsen_sandic@hotmail.com>
2019-12-11 23:05:12 +0100

    a

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var usersRouter = require('./routes/users');
 var votersRouter = require('./routes/voters');
 var http = require('http');
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = 3000;
 app.set('port', port);
 
 
@@ -62,23 +62,6 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
 
 /**
  * Event listener for HTTP server "error" event.

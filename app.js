22ac467842a359f144db4c6c605e7d2299c43fec
unknown <arsen_sandic@hotmail.com>
2019-12-12 00:00:32 +0100

    a

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ var usersRouter = require('./routes/users');
 var votersRouter = require('./routes/voters');
 var http = require('http');
 var debug = require('debug')('newproject:server');
-
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
 var port = 3000;
 
 

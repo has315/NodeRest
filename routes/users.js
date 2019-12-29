bf1d9cec1c2a74fa5d156382bdbaee8619539610
unknown <arsen_sandic@hotmail.com>
2019-12-29 23:12:06 +0100

    a

--- routes/users.js
+++ routes/users.js
@@ -22,8 +22,8 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
   let data = {
     username: req.body.username,
-    salt: bcrypt.genSaltSync(saltRounds),
-    password: bcrypt.hashSync(req.body.password, salt)
+    password = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+
   };
   console.log(data);
 

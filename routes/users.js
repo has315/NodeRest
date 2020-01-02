9d41ac53077c63990cd8dd92268c3842388e422c
unknown <arsen_sandic@hotmail.com>
2020-01-02 23:24:23 +0100

    a

--- routes/users.js
+++ routes/users.js
@@ -96,7 +96,7 @@ router.post('/login', function (req, res, next) {
         res.send(JSON.stringify({
           "status": 200,
           "error": null,
-          "response": results
+          "response": 2
         }));
       }
     });

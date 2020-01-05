24c99902d49cca4fd9e5f9b0b2c35ad28c9414b7
unknown <arsen_sandic@hotmail.com>
2020-01-05 20:45:50 +0100

    a

--- test/zombie.js
+++ test/zombie.js
@@ -5,7 +5,7 @@ let zombie = {
 
     get_cik: (data)  => {
         let person = data;
-        browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3", () => {
+        browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3", function() {
             console.log(browser.text("title"));
 
             let first_name = person.first_name;

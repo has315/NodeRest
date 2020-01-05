bd6e22ac3e787077773321313cf0871cbaa884ed
unknown <arsen_sandic@hotmail.com>
2020-01-05 19:46:42 +0100

    a

--- test/zombie.js
+++ test/zombie.js
@@ -1,46 +1,50 @@
 const Browser = require('zombie');
 browser = new Browser()
 
+let zombie = {
 
-function get_cik(data) {
-    browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3", () => {
-        console.log(browser.text("title"));
+    data: function get_cik(data) {
+        browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3", () => {
+            console.log(browser.text("title"));
 
-        let first_name = data.first_name;
-        let last_name = data.last_name;
-        let jmbg = data.jmbg;
-        
-
-        browser.fill("#Prezime", first_name);
-        browser.fill("#Ime", last_name);
-        browser.fill("#JMBG", jmbg);
-
-        browser.pressButton("#ctl04_cmdProvjeri", () => {
-            console.log("Form submit ok");
-            console.log()
-
-            let label1 = browser.document.getElementsByClassName("Label1");
-            let label2 = browser.document.getElementsByClassName("Label2");
-
-            if (label1.length == label2.length) {
-                // Access response data
-                let result = {};
-                result[label1[i].innerHTML] = label2[i].innerHTML;
-                
-
-                person.voting_location = result[4];
-                person.voting_location_address = result[5];
-                person.voting_location.name = result[6]
-                person.voting_location.municipality = result[7];
-            
-            }
+            let first_name = data.first_name;
+            let last_name = data.last_name;
+            let jmbg = data.jmbg;
 
 
+            browser.fill("#Prezime", first_name);
+            browser.fill("#Ime", last_name);
+            browser.fill("#JMBG", jmbg);
+
+            browser.pressButton("#ctl04_cmdProvjeri", () => {
+                console.log("Form submit ok");
+                console.log()
+
+                let label1 = browser.document.getElementsByClassName("Label1");
+                let label2 = browser.document.getElementsByClassName("Label2");
+
+                if (label1.length == label2.length) {
+                    // Access response data
+                    let result = {};
+                    result[label1[i].innerHTML] = label2[i].innerHTML;
+
+
+                    person.voting_location = result[4];
+                    person.voting_location_address = result[5];
+                    person.voting_location.name = result[6]
+                    person.voting_location.municipality = result[7];
+
+                }
+
+
+            })
+
+            return person;
         })
+    }
 
-        return person;
-    })
 }
 
-module.exports = browser;
 
+
+module.exports = zombie;

ffd14ec9593ae30c71faecad1e7267956b805299
unknown <arsen_sandic@hotmail.com>
2020-01-05 22:57:46 +0100

    a

--- test/zombie.js
+++ test/zombie.js
@@ -38,13 +38,13 @@ let zombie = {
                     person.voting_location_municipality = result[6];
                 }
                 
-                // let sql = `UPDATE vote SET voting_location = '${person.voting_location}', voting_location_address = '${person.voting_location_address}', voting_location_name = '${person.voting_location_name}', voting_location_municipality = '${person.voting_location_municipality}' WHERE jmbg = '${person.jmbg}'`;
-                // connection.query(sql, person, (err, results) => {
-                //     if (err) throw err;
-                // })
+                let sql = `UPDATE vote SET voting_location = '${person.voting_location}', voting_location_address = '${person.voting_location_address}', voting_location_name = '${person.voting_location_name}', voting_location_municipality = '${person.voting_location_municipality}' WHERE jmbg = '${person.jmbg}'`;
+                connection.query(sql, person, (err, results) => {
+                    if (err) throw err;
+                })
             })
         });
-        return result;
+        return person;
     }
 
 }

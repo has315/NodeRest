e85c7d27efaaa18599c2882aa9b2a46be05ce248
unknown <arsen_sandic@hotmail.com>
2020-01-05 21:54:46 +0100

    a

--- routes/voters.js
+++ routes/voters.js
@@ -3,17 +3,22 @@ var router = express.Router();
 var connection = require('../db');
 var zombie = require('../test/zombie');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let sql = 'SELECT * from vote';
-	  connection.query(sql, function (error, results, fields) {
-		if (error) throw error;
-		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-	});
+  connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify({
+      "status": 200,
+      "error": null,
+      "response": results
+    }));
+  });
 });
 
-router.post('/', function(req, res, next){
+router.post('/', function (req, res, next) {
 
-  let data  = {
+
+  let data = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     jmbg: req.body.jmbg,
@@ -22,13 +27,9 @@ router.post('/', function(req, res, next){
     added: req.body.added
   };
 
-   let aa = zombie.get_cik(data);
-
-   console.log(aa);
-
   let sql = "INSERT INTO vote SET ?";
-    connection.query(sql,aa, (err, results) => {
-    if(err) throw err;
+  connection.query(sql, data, (err, results) => {
+    if (err) throw err;
     res.send(JSON.stringify({
       "status": 200,
       "error": null,
@@ -41,7 +42,7 @@ router.get('/search', function (req, res, next) {
   data = {
     username: req.query.value
   }
-//'SELECT * FROM `user` WHERE `username` LIKE  \'?%\''
+  //'SELECT * FROM `user` WHERE `username` LIKE  \'?%\''
   connection.query({
     sql: `SELECT * FROM vote WHERE ${req.query.key} LIKE '${req.query.value}%'`
   }, (error, results, fields) => {
--- test/zombie.js
+++ test/zombie.js
@@ -1,93 +1,59 @@
 const Browser = require('zombie');
-const mocha = require('mocha');
-const describe = mocha.describe;
+var connection = require('../db');
+
 browser = new Browser()
 
+
 let zombie = {
-    get_cik: function () {
-        describe('Visiting CIK page', function (person) {
-            const browser = new Browser();
-            before(function () {
-                return browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3");
-            });
 
-            describe('Submiting form', function () {
-                before(function () {
-                    let first_name = person.first_name;
-                    let last_name = person.last_name;
-                    let jmbg = person.jmbg;
+    get_cik: (person) => {
+        let person = data;
+        browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3").then(() => {
+            console.log(browser.text("title"));
 
-                    browser.fill("#Prezime", first_name);
-                    browser.fill("#Ime", last_name);
-                    browser.fill("#JMBG", jmbg);
-                    return browser.pressButton('Sign Me Up!');
-                });
-            });
+            let first_name = person.first_name;
+            let last_name = person.last_name;
+            let jmbg = person.jmbg;
 
-            console.log("Form submit ok");
+            browser.fill("#Prezime", first_name);
+            browser.fill("#Ime", last_name);
+            browser.fill("#JMBG", jmbg);
 
-            let label1 = browser.document.getElementsByClassName("Label1");
-            let label2 = browser.document.getElementsByClassName("Label2");
+            browser.document.forms[0].submit();
 
-            // Access response data
-            let result = {};
-            for (let i = 0; i < label1.length; ++i)
-                result[label1[i].innerHTML] = label2[i].innerHTML;
+            browser.wait(() => {
+                console.log("Form submit ok");
 
-            if (label1.length == label2.length) {
+                let label1 = browser.document.getElementsByClassName("Label1");
+                let label2 = browser.document.getElementsByClassName("Label2");
+
                 // Access response data
-                person.voting_location = result[4];
-                person.voting_location_address = result[5];
-                person.voting_location_name = result[6]
-                person.voting_location_municipality = result[7];
-            }
+                let result = {};
+                for (let i = 0; i < label1.length; ++i)
+                    result[label1[i].innerHTML] = label2[i].innerHTML;
 
+                if (label1.length == label2.length) {
+                    // Access response data
+                    person.voting_location = result[4];
+                    person.voting_location_address = result[5];
+                    person.voting_location_name = result[6]
+                    person.voting_location_municipality = result[7];
+                }
+                let sql = `UPDATE vote SET ? WHERE jmbg == ${person.jmbg}`;
+                connection.query(sql, data, (err, results) => {
+                    if (err) throw err;
+                    res.send(JSON.stringify({
+                        "status": 200,
+                        "error": null,
+                        "response": results
+                    }));
+                })
+            })
         });
+        return person;
     }
+
 }
-
-
-// let zombie = {
-
-//     get_cik: async (data) => {
-//         let person = data;
-//         browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3").then(() => {
-//             console.log(browser.text("title"));
-
-//             let first_name = person.first_name;
-//             let last_name = person.last_name;
-//             let jmbg = person.jmbg;
-
-//             browser.fill("#Prezime", first_name);
-//             browser.fill("#Ime", last_name);
-//             browser.fill("#JMBG", jmbg);
-
-//             browser.document.forms[0].submit();
-
-//             browser.wait(() => {
-//                 console.log("Form submit ok");
-
-//                 let label1 = browser.document.getElementsByClassName("Label1");
-//                 let label2 = browser.document.getElementsByClassName("Label2");
-
-//                 // Access response data
-//                 let result = {};
-//                 for (let i = 0; i < label1.length; ++i)
-//                     result[label1[i].innerHTML] = label2[i].innerHTML;
-
-//                 if (label1.length == label2.length) {
-//                     // Access response data
-//                     person.voting_location = result[4];
-//                     person.voting_location_address = result[5];
-//                     person.voting_location_name = result[6]
-//                     person.voting_location_municipality = result[7];
-//                 }
-//             })
-//         });
-//         return person;
-//     }
-
-// }
 
 
 

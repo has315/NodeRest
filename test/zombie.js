9d13605f6d921b45625161fc7fc12ed8d4f275e1
unknown <arsen_sandic@hotmail.com>
2020-01-05 22:15:25 +0100

    a

--- test/zombie.js
+++ test/zombie.js
@@ -38,6 +38,8 @@ let zombie = {
                     person.voting_location_name = result[6]
                     person.voting_location_municipality = result[7];
                 }
+
+                console.log(person);
                 let sql = `UPDATE vote SET voting_location = '${person.voting_location}', voting_location_address = '${person.voting_location_address}', voting_location_name = '${person.voting_location_name}', voting_location_municipality = '${person.voting_location_municipality}' WHERE jmbg = '${person.jmbg}'`;
                 connection.query(sql, person, (err, results) => {
                     if (err) throw err;

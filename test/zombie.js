1cf1b1bca9027a50b4a606017aa56cf3a7d6e827
unknown <arsen_sandic@hotmail.com>
2020-01-05 23:03:45 +0100

    a

--- test/zombie.js
+++ test/zombie.js
@@ -6,13 +6,13 @@ browser = new Browser()
 
 let zombie = {
 
-    get_cik: (person) => {
+    get_cik: (data) => {
         browser.visit("https://www.izbori.ba/Default.aspx?CategoryID=509&Lang=3").then(() => {
             console.log(browser.text("title"));
 
-            let first_name = person.first_name;
-            let last_name = person.last_name;
-            let jmbg = person.jmbg;
+            let first_name = data.first_name;
+            let last_name = data.last_name;
+            let jmbg = data.jmbg;
 
             browser.fill("#Prezime", first_name);
             browser.fill("#Ime", last_name);
@@ -38,8 +38,8 @@ let zombie = {
                     data.voting_location_municipality = result[6];
                 }
                 console.log(result);
-                let sql = `UPDATE vote SET voting_location = '${person.voting_location}', voting_location_address = '${person.voting_location_address}', voting_location_name = '${person.voting_location_name}', voting_location_municipality = '${person.voting_location_municipality}' WHERE jmbg = '${person.jmbg}'`;
-                connection.query(sql, person, (err, results) => {
+                let sql = `UPDATE vote SET voting_location = '${data.voting_location}', voting_location_address = '${data.voting_location_address}', voting_location_name = '${data.voting_location_name}', voting_location_municipality = '${data.voting_location_municipality}' WHERE jmbg = '${data.jmbg}'`;
+                connection.query(sql, data, (err, results) => {
                     if (err) throw err;
                 })
             })

e1637550f12a7aec651d4735db601b59bfab4590
zrilman <dragec.96@live.com>
2020-01-06 13:20:46 +0100

    a

--- routes/voters.js
+++ routes/voters.js
@@ -3,18 +3,18 @@ var router = express.Router();
 var connection = require('../db');
 var zombie = require('../test/zombie');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let sql = 'SELECT * from vote';
-	  connection.query(sql, function (error, results, fields) {
-		if (error) throw error;
-		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-	});
+  connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+  });
 });
 
-router.post('/', function(req, res, next){
+router.post('/', function (req, res, next) {
 
-  
-  let data  = {
+
+  let data = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     jmbg: req.body.jmbg,
@@ -22,12 +22,13 @@ router.post('/', function(req, res, next){
     delegated: req.body.delegated,
     added: req.body.added
   };
-  
- var aa = zombie.get_cik(data);
-console.log(aa);
+
+  var aa = zombie.get_cik(data);
+  console.log(aa);
+
   let sql = "INSERT INTO vote SET ?";
-    connection.query(sql,data, (err, results) => {
-    if(err) throw err;
+  connection.query(sql, aa, (err, results) => {
+    if (err) throw err;
     res.send(JSON.stringify({
       "status": 200,
       "error": null,
@@ -40,7 +41,7 @@ router.get('/search', function (req, res, next) {
   data = {
     username: req.query.value
   }
-//'SELECT * FROM `user` WHERE `username` LIKE  \'?%\''
+  //'SELECT * FROM `user` WHERE `username` LIKE  \'?%\''
   connection.query({
     sql: `SELECT * FROM vote WHERE ${req.query.key} LIKE '${req.query.value}%'`
   }, (error, results, fields) => {

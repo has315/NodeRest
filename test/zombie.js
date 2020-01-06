937e28ac5588710072a28fa1c15ada80e449997c
zrilman <dragec.96@live.com>
2020-01-06 13:13:51 +0100

    a

--- test/zombie.js
+++ test/zombie.js
@@ -29,7 +29,7 @@ let zombie = {
                 // Access response data
                 let result = {};
                 for (let i = 0; i < label1.length; ++i)
-                    result[label1[i].innerHTML.replace(":", "").trim()] = label2[i].innerHTML;
+                    result[label1[i].innerHTML.replace(":", "").trim()] = label2[i].innerHTML.trim();
 
                 if (label1.length == label2.length) {
                     // Access response data

335610f2ad7e7875c4f9646f02faf128f996316c
unknown <arsen_sandic@hotmail.com>
2020-01-19 19:54:11 +0100

    a

--- routes/voters.js
+++ routes/voters.js
@@ -127,13 +127,14 @@ router.post('/', function (req, res, next) {
   };
 
 
+  let sql = "SELECT EXISTS(SELECT * FROM vote WHERE `jmbg` =  ?)";
   connection.query(sql, data.jmbg, (err, results) => {
     if (err) throw err;
     // If insert was successful get cik data
     zombie.get_cik(data);
     res.status(HttpStatus.OK).send(JSON.stringify({
       "error": null,
-      "response": results.slice(-2),
+      "response": results.slice(-2);
     }));
   });
 });

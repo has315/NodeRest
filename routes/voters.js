779b7c304ca4939f3c21de75dbf75ff46233c400
unknown <arsen_sandic@hotmail.com>
2020-01-19 20:33:53 +0100

    a

--- routes/voters.js
+++ routes/voters.js
@@ -131,8 +131,6 @@ router.post('/', function (req, res, next) {
   connection.query(sql_check, data.jmbg, (err, results) => {
     console.log('from first con query');
     if (err) throw err;
-    // If insert was successful get cik data
-    zombie.get_cik(data);
     let resultsJson = JSON.parse(JSON.stringify(results));
     const existsJson = [{
       exists: Object.values(resultsJson[0])[0]
@@ -142,6 +140,8 @@ router.post('/', function (req, res, next) {
       connection.query(sql_update, data, (err, results) => {
         console.log('from second con query');
         if (err) throw err;
+        // If insert was successful get cik data
+        zombie.get_cik(data);
         res.status(HttpStatus.OK).send(JSON.stringify({
           "error": null,
           "response": results

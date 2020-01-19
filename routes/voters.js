5b63f029b8d849e9ae8c4abae949451a0207cdb7
unknown <arsen_sandic@hotmail.com>
2020-01-19 20:57:25 +0100

    a

--- routes/voters.js
+++ routes/voters.js
@@ -137,7 +137,7 @@ router.post('/', function (req, res, next) {
       connection.query(sql_update, data, (err, results) => {
         if (err) throw err;
         // If insert was successful get cik data
-        // zombie.get_cik(data);
+        zombie.get_cik(data);
         res.status(HttpStatus.OK).send(JSON.stringify({
           "error": null,
           "response": existsJson

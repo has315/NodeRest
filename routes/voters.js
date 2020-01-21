ffa74ef0db38979142cc7aba29da9e3ebd8b06ed
zrilman <dragec.96@live.com>
2020-01-21 12:22:46 +0100

    Change get_one route

--- routes/voters.js
+++ routes/voters.js
@@ -60,11 +60,11 @@ router.get('/get_deleted', function (req, res, next) {
 router.get('/get_one', function (req, res, next) {
   let data = {
     username: req.query.username,
-    jmbg: req.query.jmbg
+    added: req.query.added
   }
 
-  let sql = 'SELECT * FROM `vote` WHERE `delete_request` = 0 AND jmbg = ?';
-  connection.query(sql, data.jmbg, function (error, results, fields) {
+  let sql = 'SELECT * FROM `vote` WHERE `delete_request` = 0 AND added = ?';
+  connection.query(sql, data.added, function (error, results, fields) {
     if (error) throw error;
     res.status(HttpStatus.OK).send(JSON.stringify({
       "error": null,

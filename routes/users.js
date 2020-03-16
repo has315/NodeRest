53f9f1c50b12d4110f8e720779186cf4e2a68d7d
unknown <arsen_sandic@hotmail.com>
2020-03-16 13:25:36 +0100

    a

--- routes/users.js
+++ routes/users.js
@@ -87,25 +87,14 @@ router.post('/login', function (req, res, next) {
       
       if (success) {
         // Generate JWT
-        const token = jwt.sign({ id: results[0].id }, AppConfig.SECRET, { expiresIn: AppConfig.TOKEN_LIFESPAN });
-        const refreshToken = jwt.sign({ id: results[0].id }, AppConfig.REFRESH_TOKEN_SECRET, { expiresIn: AppConfig.REFRESH_TOKEN_LIFESPAN });
-        const fun = function (err, reply) {
-          if (err)
-            throw err;
-          if (reply){
-            //
-          }
-        };
-        // Store refreshToken in Redis
-        redisClient.set("key", "value", fun);
-        redisClient.hmset(HSET, results[0].id, refreshToken, fun);
+        const token = jwt.sign({ id: results[0].id }, AppConfig.SECRET);
+       
 
         // Send response
         res.status(HttpStatus.OK).send(JSON.stringify({
           "error": null,
           "response": results[0].account_level,
           "token": token,
-          "refreshToken": refreshToken,
         }));
       } else {
         res.status(HttpStatus.UNAUTHORIZED).send(JSON.stringify({

6b2bdaaa6da0958cc815513b2b1e5ec4d0fdfeab
unknown <arsen_sandic@hotmail.com>
2020-03-26 21:10:17 +0100

    a

--- app.js
+++ app.js
@@ -10,13 +10,15 @@ var cors = require('cors');
 var http = require('http');
 var debug = require('debug')('newproject:server');
 var bodyParser = require('body-parser');
-
 var port = 3000;
-
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 var app = express();
 
 app.use(logger('dev'));
-app.use(cors);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false

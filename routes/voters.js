04499239e8225579eda8e52954d91ff1e8caa21b
unknown <arsen_sandic@hotmail.com>
2020-05-25 21:45:15 +0200

    a

--- routes/voters.js
+++ routes/voters.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const connection = require('../db/mysql');
 const zombie = require('../test/zombie');
 const auth = require('../middleware/auth');
-const logger = require('logger').createLogger(`..${AppConfig.LOG_DIR}/voters.logs`);
+const logger = require('logger').createLogger(`${AppConfig.LOG_DIR}/voters.logs`);
 
 
 // function isValidVoter(vote)   {
@@ -27,7 +27,7 @@ const logger = require('logger').createLogger(`..${AppConfig.LOG_DIR}/voters.log
 
 
 // GET ALL NON DELETED VOTES
-router.get('/all', auth.authUser, function (req, res, next) {
+router.get('/all', auth.authUser, function(req, res, next) {
     if (!req.query.hasOwnProperty('id') || !req.query.id) {
         return res.status(HttpStatus.BAD_REQUEST).json({
             status: 'Error',
@@ -65,11 +65,11 @@ router.get('/all', auth.authUser, function (req, res, next) {
 });
 
 // GET ALL EDITED VOTES
-router.get('/get_edit', auth.authAdmin, function (req, res, next) {
+router.get('/get_edit', auth.authAdmin, function(req, res, next) {
     let sql = 'SELECT * FROM `vote_edit_full_view`';
-    connection.query(sql, function (error, results, fields) {
+    connection.query(sql, function(error, results, fields) {
         if (error) throw error;
-        connection.query(sql, function (error, results, fields) {
+        connection.query(sql, function(error, results, fields) {
             if (error) {
                 logger.error("UNABLE TO GET ALL EDITED VOTES")
                 throw error;
@@ -82,9 +82,9 @@ router.get('/get_edit', auth.authAdmin, function (req, res, next) {
     });
 });
 // GET ALL DELETED VOTES
-router.get('/get_deleted', auth.authAdmin, function (req, res, next) {
+router.get('/get_deleted', auth.authAdmin, function(req, res, next) {
     let sql = 'SELECT * FROM `vote` WHERE `delete_request` = 1';
-    connection.query(sql, function (error, results, fields) {
+    connection.query(sql, function(error, results, fields) {
         if (error) throw error;
         res.status(HttpStatus.OK).send(JSON.stringify({
             "error": null,
@@ -94,14 +94,14 @@ router.get('/get_deleted', auth.authAdmin, function (req, res, next) {
 });
 
 // GET ONE VOTE RECORD 
-router.get('/get_one', auth.authUser, function (req, res, next) {
+router.get('/get_one', auth.authUser, function(req, res, next) {
     let data = {
         username: req.query.username,
         vote_id: req.query.vote_id
     }
 
     let sql = "SELECT * FROM `vote` WHERE `delete_request` = 0 AND vote_id = ?";
-    connection.query(sql, data.vote_id, function (error, results, fields) {
+    connection.query(sql, data.vote_id, function(error, results, fields) {
         if (error) throw error;
         res.status(HttpStatus.OK).send(JSON.stringify({
             "error": null,
@@ -111,7 +111,7 @@ router.get('/get_one', auth.authUser, function (req, res, next) {
 });
 
 // INSERT EDITED VOTE
-router.post('/edit_request', auth.authUser, function (req, res, next) {
+router.post('/edit_request', auth.authUser, function(req, res, next) {
     let data = {
         vote_id: req.body.vote_id,
         first_name: req.body.first_name,
@@ -133,7 +133,7 @@ router.post('/edit_request', auth.authUser, function (req, res, next) {
 });
 
 // UPDATE VOTE
-router.post('/update', auth.authAdmin, function (req, res, next) {
+router.post('/update', auth.authAdmin, function(req, res, next) {
     let data = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
@@ -165,7 +165,7 @@ router.post('/update', auth.authAdmin, function (req, res, next) {
 
 
 // DECLINE AND DELETE VOTE EDIT ENTRY
-router.post('/edit_request_delete', auth.authAdmin, function (req, res, next) {
+router.post('/edit_request_delete', auth.authAdmin, function(req, res, next) {
     let data = {
         vote_id: req.body.vote_id
     }
@@ -182,7 +182,7 @@ router.post('/edit_request_delete', auth.authAdmin, function (req, res, next) {
 
 
 // INSERT NEW VOTE
-router.post('/', auth.authUser, function (req, res, next) {
+router.post('/', auth.authUser, function(req, res, next) {
     let data = {
         vote_id: req.body.user_id,
         first_name: req.body.first_name,
@@ -228,7 +228,7 @@ router.post('/', auth.authUser, function (req, res, next) {
 });
 
 // SEARCH FUNCTIONALITY
-router.get('/search', auth.authUser, function (req, res, next) {
+router.get('/search', auth.authUser, function(req, res, next) {
     // Escape input to prevent SQL Injection
     let data = {
         key: connection.escape(req.query.key).replace(/'/g, ""),
@@ -259,7 +259,7 @@ router.get('/search', auth.authUser, function (req, res, next) {
 });
 
 // SETS FLAG FOR DELETION = 1
-router.post('/delete_request', auth.authUser, function (req, res, next) {
+router.post('/delete_request', auth.authUser, function(req, res, next) {
     let data = {
         jmbg: req.body.jmbg
     };
@@ -275,7 +275,7 @@ router.post('/delete_request', auth.authUser, function (req, res, next) {
 });
 
 // DELETE FROM VOTE
-router.post('/delete', auth.authAdmin, function (req, res, next) {
+router.post('/delete', auth.authAdmin, function(req, res, next) {
     let data = {
         vote_id: req.body.vote_id
     };
@@ -292,7 +292,7 @@ router.post('/delete', auth.authAdmin, function (req, res, next) {
 });
 
 // DECLINE DELETE AND  SETS DELETION FLAG TO 0
-router.post('/delete_decline', auth.authAdmin, auth.authUser, function (req, res, next) {
+router.post('/delete_decline', auth.authAdmin, auth.authUser, function(req, res, next) {
     let data = {
         vote_id: req.body.vote_id
     };
